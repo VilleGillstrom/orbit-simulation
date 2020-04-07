511c83f6afb978f1604a42d9c15c7383409d8800
Villegillstrom <ville.gillstrom@gmail.com>
2020-04-07 20:05:10 +0200

    Remove unused file

--- plotting/Nbody_Ep_plot.m
+++
@@ -1,7 +0,0 @@
-function [outputArg1,outputArg2] = Nbody_Ep_plot(inputArg1,inputArg2)
-%NBODY_EP_PLOT Summary of this function goes here
-%   Detailed explanation goes here
-outputArg1 = inputArg1;
-outputArg2 = inputArg2;
-end
-
