6e9d26aee52f37d2e7df111437cc09745e33b318
Villegillstrom <ville.gillstrom@gmail.com>
2020-04-09 08:38:37 +0200

    Fixed more comments

--- helpers/vel_for_static_com.m
+++ helpers/vel_for_static_com.m
@@ -1,12 +1,17 @@
 function [Vx, Vy] = vel_for_static_com(M,m,vx, vy)
-%UNTITLED What is the velocity needed for a body to make the system's
-%center of mass static
+%VEL_FOR_STATIC_COM Get the velocity needed for a body with mass M 
+% to make the system's center of mass static
 %   input:
 %   M - mass of body
-%   m - mass of bodies 
+%   m - mass of all bodies 
 %   v - velocity of all bodies
 %   output:
 %   V- velocity for M to achieve static center of mass
+%
+%   mwe: To get velocity for a body with M = 10 to make the center 
+%   of mass static in a system with two existing bodies
+%   [vx, vy] = vel_for_static_com(10,[5, 1],[-0.2,0], [0,0.75])
+%   
     mvx = m*(vx).'; 
     mvy = m*(vy).';
     Vx = -mvx / M;
